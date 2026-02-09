3ffed5154a8f442ce377ac960cb7092fcbcfef34
asharamanujam <103427149+asharamanujam@users.noreply.github.com>
2026-02-09 17:35:08 -0500

    Update full_space_opt.py

--- Generation and Transmission Expansion Planning(GTEPEnv)/full_space_opt.py
+++ Generation and Transmission Expansion Planning(GTEPEnv)/full_space_opt.py
@@ -26,14 +26,16 @@ def build_optimization_model(problem_data = problem_data):
     trans_dict = {l:tuple(l.split('_')) for l in transmission_lines}
     T = problem_data["T"]
     t_init = problem_data["t_init"]
+    time_all = range(t_init, t_init + T + 1)
+    time_add = range(t_init + 1, t_init + T + 1)
     num_gen_0 = {(i,r):0 for i in generators for r in regions}
     bin_trans_0 = {l:0 for l in transmission_lines}
     model = ConcreteModel()
-    model.num_gen = Var(generators,regions,range(t_init,t_init+T+1),domain = Integers)
-    model.add_gen = Var(generators,regions,range(t_init,t_init+T+1),domain = Integers)
+    model.num_gen = Var(generators,regions,time_all,domain = Integers)
+    model.add_gen = Var(generators,regions,time_all,domain = Integers)
     has_transmission = len(transmission_lines) > 0
-    model.num_gen = Var(generators,regions,range(t_init,t_init+T+1),domain = Integers)
-    model.add_gen = Var(generators,regions,range(t_init,t_init+T+1),domain = Integers)
+    model.num_gen = Var(generators,regions,time_all,domain = Integers)
+    model.add_gen = Var(generators,regions,time_all,domain = Integers)
     if has_transmission:
         model.pow_flow = Var(transmission_lines, time_all)  # Reals by default
         model.bin_trans = Var(transmission_lines, time_all, domain=Binary)
@@ -48,32 +50,32 @@ def build_optimization_model(problem_data = problem_data):
     model.init_addgen = Constraint(generators,regions, rule=init_addgen_rule)
     def lb_numgen_rule(model,i,r,t):
         return model.num_gen[i,r,t]>=0
-    model.lb_numgen = Constraint(generators,regions,range(t_init,t_init+T+1),rule = lb_numgen_rule)
+    model.lb_numgen = Constraint(generators,regions,time_all,rule = lb_numgen_rule)
     def ub_numgen_rule(model,i,r,t):
         return model.num_gen[i,r,t]<=env_config["maxgen"][i][r]
-    model.ub_numgen = Constraint(generators,regions,range(t_init,t_init+T+1),rule = ub_numgen_rule)
+    model.ub_numgen = Constraint(generators,regions,time_all,rule = ub_numgen_rule)
     if has_transmission:
         def init_bin_trans_rule(model, l):
             return model.bin_trans[l,t_init] == bin_trans_0[l]   
         model.init_bintrans = Constraint(transmission_lines, rule=init_bin_trans_rule)
         def lb_powflow_rule(model,l,t):
             return model.pow_flow[l,t]>=-env_config["tlcap"][l]*model.bin_trans[l,t]
-        model.lb_powflow = Constraint(transmission_lines,range(t_init,t_init+T+1),rule = lb_powflow_rule)
+        model.lb_powflow = Constraint(transmission_lines,time_all,rule = lb_powflow_rule)
         def ub_powflow_rule(model,l,t):
             return model.pow_flow[l,t]<=env_config["tlcap"][l]*model.bin_trans[l,t]
-        model.ub_powflow = Constraint(transmission_lines,range(t_init,t_init+T+1),rule = ub_powflow_rule)
+        model.ub_powflow = Constraint(transmission_lines,time_all,rule = ub_powflow_rule)
         def lb_bin_trans_rule(model,l,t):
             return model.bin_trans[l,t]>=model.bin_trans[l,t-1]
-        model.lb_bin_trans = Constraint(transmission_lines,range(t_init+1,t_init+T+1),rule = lb_bin_trans_rule)
+        model.lb_bin_trans = Constraint(transmission_lines,time_add,rule = lb_bin_trans_rule)
         def val_add_trans_rule(model,l,t):
             return model.bin_trans_add[l,t] == model.bin_trans[l,t]-model.bin_trans[l,t-1]
-        model.val_add_trans = Constraint(transmission_lines,range(t_init+1,t_init+T+1),rule = val_add_trans_rule)
+        model.val_add_trans = Constraint(transmission_lines,time_add,rule = val_add_trans_rule)
     def lb_addgen_rule(model,i,r,t):
         return model.add_gen[i,r,t]>=0
-    model.lb_addgen = Constraint(generators,regions,range(t_init,t_init+T+1),rule = lb_addgen_rule)
+    model.lb_addgen = Constraint(generators,regions,time_all,rule = lb_addgen_rule)
     def state_rule(model,i,r,t):
         return model.num_gen[i,r,t]==model.num_gen[i,r,t-1]+model.add_gen[i,r,t]
-    model.state = Constraint(generators,regions,range(t_init+1,t_init+T+1),rule = state_rule)
+    model.state = Constraint(generators,regions,time_add,rule = state_rule)
     def demand_check_rule(model, r, t):
         demand_t = env_config["demand"][r].get(str(t), 0) if t <= env_config["T"] else 0
         gen_supply = sum(model.num_gen[i, r, t] * env_config["gencap"][i] for i in generators)
@@ -86,13 +88,13 @@ def build_optimization_model(problem_data = problem_data):
         outflow = sum(model.pow_flow[l, t] for l in transmission_lines if r == trans_dict[l][0])
         return demand_t <= gen_supply + inflow - outflow
 
-    model.demand_check = Constraint(regions, range(t_init+1,t_init+T+1), rule=demand_check_rule)
+    model.demand_check = Constraint(regions, time_add, rule=demand_check_rule)
     def objective_rule(model):
         gen_cost = -sum(
             model.add_gen[i, r, t] * env_config["installcost"]["generators"][i]
             for i in generators
             for r in regions
-            for t in range(t_init,t_init+T+1)
+            for t in time_all
         )
 
         if not has_transmission:
@@ -101,9 +103,10 @@ def build_optimization_model(problem_data = problem_data):
         trans_cost = -sum(
             model.bin_trans_add[l, t] * env_config["installcost"]["transmission"][l]
             for l in transmission_lines
-            for t in range(t_init,t_init+T+1)
+            for t in time_all
         )
         return gen_cost + trans_cost
 
     model.obj = Objective(rule=objective_rule, sense=maximize)
     return model
+
