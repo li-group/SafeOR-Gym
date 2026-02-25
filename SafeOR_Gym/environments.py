937f1f7cfbe78449697df16fd70c2dc411eb867c
asharamanujam <103427149+asharamanujam@users.noreply.github.com>
2026-02-25 15:54:03 -0500

    Add files via upload

--- SafeOR_Gym/environments.py
+++ SafeOR_Gym/environments.py
@@ -1,11 +1,11 @@
 Env_dict = {
-    'Production Scheduling in Air Separation Unit (ASUEnv)':['ASUEnv',['ASU1']],
-    'Generation and Transmission Expansion Planning(GTEPEnv)' : ['Generator_transmission_expansion_env',['Capacity-Expansion']],
-    'Grid Integrated Energy Storage (GridStorageEnv)': ['BatteryOperationEnv',['Battery-v0']],
-    'Integrated scheduling and maintenance (SchedMaintEnv)': ['GASU',['GASU-v0']],
-    'Multi-Echelon Supply Chain (InvMgmtEnv)': ['InvMgmtEnv',['SupplyChain-v0']],
-    'Multiperiod Blending Problem(BlendingEnv)': ['BlendEnv',['Blending-simple']],
-    'Resource Task Network':['RTNEnv',['rtn-v0']],
-    'State Task Network':['STNEnv',['stn-v0']],
-    'UnitCommitment' : ['UnitCommitmentMasterEnv',['UC-v0','UC-v1']]
+    'Production Scheduling in Air Separation Unit (ASUEnv)':['ASUEnv',['ASU1'],'asuenv_config.json'],
+    'Generation and Transmission Expansion Planning(GTEPEnv)' : ['Generator_transmission_expansion_env',['Capacity-Expansion'],'gen_trans_config.json'],
+    'Grid Integrated Energy Storage (GridStorageEnv)': ['BatteryOperationEnv',['Battery-v0'],'battery_config.json'],
+    'Integrated scheduling and maintenance (SchedMaintEnv)': ['GASU',['GASU-v0','GASU-v1'],'gasu_config.json'],
+    'Multi-Echelon Supply Chain (InvMgmtEnv)': ['InvMgmtEnv',['SupplyChain-v0'],'supply_chain_config.json'],
+    'Multiperiod Blending Problem(BlendingEnv)': ['BlendEnv',['Blending-simple'],'Blending_prop_config.json'],
+    'Resource Task Network':['RTNEnv',['rtn-v0'],'easy_environment_config.json'],
+    'State Task Network':['STNEnv',['stn-v0'],'easy_environment_config.json'],
+    'UnitCommitment' : ['UnitCommitmentMasterEnv',['UC-v0','UC-v1'],'unit_commitment_config.json']
     }
