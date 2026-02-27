de9cce3c505ee5172849815830e8ec3382ceb870
asharamanujam <103427149+asharamanujam@users.noreply.github.com>
2026-02-27 17:15:20 -0500

    Add files via upload

--- SafeOR_Gym/environments.py
+++ SafeOR_Gym/environments.py
@@ -5,7 +5,7 @@ Env_dict = {
     'Integrated scheduling and maintenance (SchedMaintEnv)': ['GASU',['GASU-v0','GASU-v1'],'gasu_config.json'],
     'Multi-Echelon Supply Chain (InvMgmtEnv)': ['InvMgmtEnv',['SupplyChain-v0'],'supply_chain_config.json'],
     'Multiperiod Blending Problem(BlendingEnv)': ['BlendEnv',['Blending-simple'],'Blending_prop_config.json'],
-    'Resource Task Network':['RTNEnv', ['rtn-v0'],'easy_zero_environment_config.json'],
-    'State Task Network':['STNEnv',['stn-v0'],'easy_zero_environment_config.json'],
+    'Resource Task Network':['RTNEnv',['rtn-v0'],'easy_environment_config.json'],
+    'State Task Network':['STNEnv',['stn-v0'],'easy_environment_config.json'],
     'UnitCommitment' : ['UnitCommitmentMasterEnv',['UC-v0','UC-v1'],'unit_commitment_config.json']
     }
